a261c0c885ea97998d9c3b78f1dee7243eecae78
yoooooni <yunijang12@gmail.com>
2023-03-15 21:33:46 +0900

    removed Hi.java and added MenuManager.java

---
+++ src/MenuManager.java
@@ -0,0 +1,12 @@
+
+public class MenuManager {
+
+	public static void main(String[] args) {
+		System.out.println("1. Add Foods");
+		System.out.println("2. Delete Foods");
+		System.out.println("3. Edit Foods");
+		System.out.println("4. View Foods");
+		System.out.println("5. Exit");
+	}
+
+}
