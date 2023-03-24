d8bb3e70c246307e37357e80eac8fd4a53f11772
yoooooni <yunijang12@gmail.com>
2023-03-24 17:51:56 +0900

    add) manage 1. add foods

--- src/MenuManager.java
+++ src/MenuManager.java
@@ -1,12 +1,39 @@
+import java.util.Scanner;
 
 public class MenuManager {
 
 	public static void main(String[] args) {
+		int num = 0;	
+		Scanner input = new Scanner(System.in);
+		
+		while (num != 6) {
 		System.out.println("1. Add Foods");
 		System.out.println("2. Delete Foods");
 		System.out.println("3. Edit Foods");
 		System.out.println("4. View Foods");
-		System.out.println("5. Exit");
+		System.out.println("5. Show a menu");
+		System.out.println("6. Exit");
+		System.out.println("Select one number between 1~6:");
+		num = input.nextInt();
+		switch(num) {
+		case 1 :
+			System.out.print("Food Name:");
+			String foodName = input.next();
+			System.out.print("Food Price:");
+			int foodPrice = input.nextInt();
+			System.out.print("Number of Food:");
+			int numberFood = input.nextInt();
+			System.out.print("Expiration date:");
+			int date = input.nextInt();
+			break;
+		case 2 :
+		case 3 :
+		case 4 : 
+			System.out.print("Food Name:");
+		    String foodName2 = input.next();
+			
+		}
+		
 	}
-
+	}
 }
